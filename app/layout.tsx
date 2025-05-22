d8e967507850689f549648f7bde673c829632f58
robiiii0 <91593374+robiiii0@users.noreply.github.com>
2025-05-22 16:42:33 +0200

    remove cookie

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
-import CookieConsentModal from "./components/CookieConsentModal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +32,6 @@ export default function RootLayout({
         {children}
         <Analytics />
         <SpeedInsights />
-        <CookieConsentModal />
       </body>
     </html>
   );
